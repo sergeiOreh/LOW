d559a2281f3292c05b69c4373995549387f58dbd
Sergei <oreshko-sergei@mail.ru>
2020-10-11 01:37:18 +0300

    low created

---
+++ src/main/java/by/lord/of/words/LowApplication.java
@@ -0,0 +1,13 @@
+package by.lord.of.words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/lord/of/words/LowApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lord.of.words;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
